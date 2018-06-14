694458606e896e2204f901c8869177688f53d31c
Rachel Cherry <bamadesigner@gmail.com>
2018-06-14 10:45:30 -0500

    Show REST requests during development

--- functions.php
+++ functions.php
@@ -4,13 +4,21 @@
  * Only certain people can see the site
  * while we set things up.
  *
+ * Using a lower priority 'parse_request'
+ * because that's where 'REST_REQUEST' is defined.
+ *
  * @TODO:
  * - Remove before launch.
  */
-add_action( 'init', function() {
+add_action( 'parse_request', function() {
 
 	// Ignore on the login page.
 	if ( 'wp-login.php' == $GLOBALS['pagenow'] ) {
+		return;
+	}
+
+	// Ignore on the REST API. We need for Printful.
+	if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
 		return;
 	}
 
@@ -24,4 +32,4 @@ add_action( 'init', function() {
 		wp_safe_redirect( 'https://wpcampus.org' );
 		exit;
 	}
-});
+}, 100 );
